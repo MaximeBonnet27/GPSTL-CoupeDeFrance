02465d77cc2131cd91fdc1941b9c3e4d05704a39
Manyanda Chitimbo <manyanda.chitimbo@safety-line.fr>
2016-03-09 15:55:09 +0100

    Small refactor

--- src/composants/Moteur.cpp
+++ src/composants/Moteur.cpp
@@ -6,21 +6,21 @@
 void Moteur::init(){
         Logger::info("Moteur::init");
         carteMoteur = new DualVNH5019MotorShield();
-        carteMoteur.moteur_droite.speed(0);
-        carteMoteur.moteur_gauche.speed(0);
+        carteMoteur->moteur_droite.speed(0);
+        carteMoteur->moteur_gauche.speed(0);
 
 }
 
 void Moteur::avancer(float puissance){
         Logger::info("Moteur::avancer");
-        carteMoteur.moteur_droite.speed(puissance);
-        carteMoteur.moteur_gauche.speed(puissance);
+        carteMoteur->moteur_droite.speed(puissance);
+        carteMoteur->moteur_gauche.speed(puissance);
 }
 
 void Moteur::reculer(float puissance){
         Logger::info("Moteur::reculer");
-        carteMoteur.moteur_droite.speed(-puissance);
-        carteMoteur.moteur_gauche.speed(-puissance);
+        carteMoteur->moteur_droite.speed(-puissance);
+        carteMoteur->moteur_gauche.speed(-puissance);
 
 }
 
@@ -28,19 +28,19 @@ void Moteur::tourner(float puissance, double angle){
         Logger::info("Moteur::tourner");
         /* TODO A préciser */
         if(angle < 0){ // On tourne a droite
-                carteMoteur.moteur_droite.speed(0);
-                carteMoteur.moteur_gauche.speed(puissance);
+                carteMoteur->moteur_droite.speed(0);
+                carteMoteur->moteur_gauche.speed(puissance);
         }
         else{ // On tourne a gauche
-                carteMoteur.moteur_droite.speed(puissance);
-                carteMoteur.moteur_gauche.speed(0);
+                carteMoteur->moteur_droite.speed(puissance);
+                carteMoteur->moteur_gauche.speed(0);
         }
 }
 
 void Moteur::stopper(){
         Logger::info("Moteur::stopper");
-        carteMoteur.moteur_droite.speed(0);
-        carteMoteur.moteur_gauche.speed(0);
+        carteMoteur->moteur_droite.speed(0);
+        carteMoteur->moteur_gauche.speed(0);
 
 }
 
